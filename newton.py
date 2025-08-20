f0308e9035f683e5c183f504dc95a799ca169fdf
jackyhsu <m0972220819@gmail.com>
2025-08-20 12:07:05 -0700

    correct the newton method stop critiria

--- newton.py
+++ newton.py
@@ -1,7 +1,5 @@
-import numpy
-
 def finite_difference_first(f, x, eps=1e-5):
-    return (f(x + eps) - f (x)) / (eps)
+    return (f(x + eps) - f(x)) / (eps)
 
 def finite_difference_second(f, x, eps=1e-5):
     return (finite_difference_first(f, x + eps) - finite_difference_first(f, x)) / (eps)
@@ -11,10 +9,10 @@ def newton_method(f, x0, tol=1e-6, iteration=100):
     for i in range(iteration):
         first_derivatives = finite_difference_first(f, x)
         second_derivatives = finite_difference_second(f, x)
-        if abs(first_derivatives) < tol:
-            print("stop iteration")
-            break
         x_t = x - first_derivatives / second_derivatives
+        if abs(x_t - x) < tol:
+            print("stop iteration")
+            return x_t
         x = x_t
-    return x
+    return x_t
             
